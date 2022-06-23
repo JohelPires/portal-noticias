5d15c43a29921fdfa3478e3de3a3eb404ec8f58e
JohelPires <johelpires@gmail.com>
2022-06-23 13:55:14 -0400

    layout of the main section

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 function Principal({ noticias }) {
+  const restOfNews = noticias.articles.map((item, idx) => {
+    if (idx > 0) {
+      return (
+        <div key={idx} className='outras'>
+          <h4>{item.title}</h4>
+          <p className='author'>por: {item.author}</p>
+          <p>{item.description}</p>
+        </div>
+      )
+    }
+  })
   return (
     <div className='principal'>
       notícias principais:
       <div className='headline'>
         <h1>{noticias.articles[0].title}</h1>
+        <p className='author'>por: {noticias.articles[0].author}</p>
         <p>{noticias.articles[0].description}</p>
       </div>
+      <div className='outras-container'>{restOfNews}</div>
     </div>
   )
 }
