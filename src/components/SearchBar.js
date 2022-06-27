26e71a379818957cfe36a104ed1914e356176a9d
JohelPires <johelpires@gmail.com>
2022-06-27 10:32:04 -0400

    controled input searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 function SearchBar() {
+  const [search, setSearch] = useState('')
+  function handleSearch(e) {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  function updateNews() {
+    console.log(search)
+  }
+
+  function handleEnter(e) {
+    if (e.key === 'Enter') {
+      updateNews()
+    }
+  }
   return (
     <div className='searchBar'>
-      [i]
-      <input type='text' placeholder='Procure a notícia'></input>
+      <input
+        className='searchInput'
+        type='text'
+        placeholder='Procure a notícia'
+        value={search}
+        onChange={(e) => handleSearch(e)}
+        onKeyDown={(e) => handleEnter(e)}
+      ></input>
+      <FaSearch onClick={updateNews} />
       {/* <p>{hojeStr}</p> */}
     </div>
   )
